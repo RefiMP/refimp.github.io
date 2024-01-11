7d14ff1f2cc9128a13ff6c776bd1fe430f405577
RefiMP <147575692+RefiMP@users.noreply.github.com>
2024-01-11 09:19:29 +0700

    Add files via upload

---
+++ UasWeb1_2105101096/index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<title>Refi Mariska</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/templatemo_style.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>		
+        <!-- 
+        Composite Template 
+        http://www.templatemo.com/preview/templatemo_444_composite
+        -->
+	</head>
+	<body id="top" data-spy="scroll" data-target=".navbar-collapse">
+
+	<!-- start templatemo navigation -->
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">Refi Mariska Putri</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#top" class="smoothScroll">HOME</a></li>
+					<li><a href="#features" class="smoothScroll">FEATURES</a></li>
+                    <li><a href="#about" class="smoothScroll">ABOUT</a></li>
+					<li><a href="#portfolio" class="smoothScroll">PORTFOLIOS</a></li>
+					<li><a href="#contact" class="smoothScroll">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- end templatemo navigation -->
+	<section id="home" class="text-center">
+	  <div class="templatemo_headerimage">
+	    <div class="flexslider">
+	      <ul class="slides">
+	        <li>
+	        	<img src="images/slider/sliderr1.jpg" alt="Slide 1">
+	        	<div class="slider-caption">
+				    <div class="templatemo_homewrapper">
+				      <h1><a href="#I'm Refi Mariska Putri"> I'm Refi Mariska Putri</a></h1>
+				      <h2>Web Design &amp; code your dream</h2>
+						<a href="#portfolio" class="smoothScroll templatemo-slider-btn btn btn-default">Learn More</a>
+				    </div>
+			  	</div>
+	        </li>
+	        <li>
+	        	<img src="images/slider/sliderr2.jpg" alt="Slide 2">
+	        	<div class="slider-caption">
+				    <div class="templatemo_homewrapper">
+				      <h1>Web UX / UI</h1>
+				      <h2>Work on all modern browsers, IE 10+</h2>
+						<a href="#team" class="smoothScroll templatemo-slider-btn btn btn-default">Meet Our Team</a>	
+				    </div>
+			  	</div>
+	        </li>
+	        <li>
+	        	<img src="images/slider/sliderr3.jpg" alt="Slide 3">
+	        	<div class="slider-caption">
+				    <div class="templatemo_homewrapper">
+				      	<h1>Special Thanks</h1>
+				      	<h2>Welcome to My Web</h2>
+						<a href="#about" class="smoothScroll templatemo-slider-btn btn btn-default">About Us</a>	
+				    </div>
+			  	</div>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>				
+	</section>	
+	<!-- start templatemo features -->
+	<section id="features" class="features text-center">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<h2>SOME FEATURES</h2>
+					<p>Composite is free responsive mobile template from <span class="blue">template</span><span class="green">mo</span>.com website. Feel free to use this layout for any website project. Please tell your friends about us. Thank you.</p>
+				</div>
+				<div class="col-md-12"></div>
+				<div class="col-sm-6 col-md-3">
+					<a href="#"><i class="fa fa-laptop"></i></a>
+						<h3>Responsive Design</h3>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes. Etiam elementum laoreet mauris. Ut rutrum feugiat neque.</p>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<a href="#"><i class="fa fa-cog"></i></a>
+						<h3>Bootstrap 3</h3>
+						<p>Etiam elementum laoreet mauris. Ut rutrum feugiat neque. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<a href="#"><i class="fa fa-font"></i></a>
+						<h3>Google Web Fonts</h3>
+						<p>Ut rutrum feugiat neque. Etiam elementum laoreet mauris. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<a href="#"><i class="fa fa-user"></i></a>
+						<h3>User Friendly</h3>
+						<p>Cum sociis natoque penatibus et magnis dis parturient montes. Etiam elementum laoreet mauris. Ut rutrum feugiat neque.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end templatemo features -->
+
+	<!-- start templatemo about -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8 text-center">
+					<h2>ABOUT US</h2>
+					<p>Etiam elementum laoreet mauris. Ut rutrum feugiat neque. Suspendisse viverra gravida nulla. Duis sed enim vitae metus nonummy venenatis.</p>
+				</div>
+				<div class="col-md-12"></div>
+				<div class="col-sm-6 col-md-6 info">
+					<h3>We are COMPOSITE</h3>
+					<h4>WEB DESIGN &AMP; DEVELOPMENT AGENCY</h4>
+					<p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum dapibus lectus 	a ligula blandit ac egestas ipsum facilisis toque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum dapibus lectus 	a ligula blandit ac egestas ipsum facilisi.</p>
+					<p>Suspendisse potenti. Etiam elementum laoreet mauris. Ut rutrum feugiat neque. Suspendisse viverra gravida nulla. Duis sed enim vitae metus nonummy venenatis. Curabitur semper rutrum sapien. Mauris luctus.</p>
+				</div>
+				<div class="col-sm-6 col-md-6 skills">
+					<span>HTML5</span>
+						<span class="pull-right">80%</span>
+							<div class="progress">
+								<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+							</div>
+					<span>Java</span>
+						<span class="pull-right">80%</span>
+							<div class="progress">
+								<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+							</div>
+					<span>Figma</span>
+						<span class="pull-right">80%</span>
+							<div class="progress">
+								<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+							</div>
+					<span>XAMPP</span>
+						<span class="pull-right">80%</span>
+							<div class="progress">
+								<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+							</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end templatemo about -->
+
+	<!-- start templatemo portfolio -->
+	<section id="portfolio">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8">
+					<h2>PORTFOLIO</h2>
+					<p>Etiam elementum laoreet mauris. Ut rutrum feugiat neque. Suspendisse viverra gravida nulla. Duis sed enim vitae metus nonummy venenatis. Curabitur semper rutrum sapien.</p>
+				</div>
+				<div class="col-md-12"></div>
+				<div class="col-sm-6 col-md-3">
+					<div class="image_thumb">
+						<img src="images/p1.jpeg" class="img-responsive" alt="work">
+							<div class="image_overlay">
+								<a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+							</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="image_thumb">
+						<img src="images/p2.jpeg" class="img-responsive" alt="work">
+							<div class="image_overlay">
+								<a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+							</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="image_thumb">
+						<img src="images/p3.jpeg" class="img-responsive" alt="work">
+							<div class="image_overlay">
+								<a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+							</div>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<div class="image_thumb">
+						<img src="images/p4.jpeg" class="img-responsive" alt="work">
+							<div class="image_overlay">
+									<a href="#"><i class="fa fa-thumbs-o-up"></i></a>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end templatemo portfolio -->
+
+	<!-- start templatemo contact -->
+	<section id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-8 text-center">
+					<form
+  						method="POST"
+                        class="contact-from" action="process.php" method="post"
+						>
+					<h2>CONTACT US</h2>
+					<p>Suspendisse potenti. Etiam elementum laoreet mauris. Ut rutrum feugiat neque. Suspendisse viverra gravida nulla. Duis sed enim vitae metus nonummy venenatis. Curabitur semper rutrum sapien.</p>
+				</div>
+				<div class="col-md-12"></div>
+				<div class="col-sm-6 col-md-4 address">
+					<h3>OUR ADDRESS</h3>
+					<p><i class="fa fa-phone"></i>  +628899234568</p>
+					<p><i class="fa fa-envelope-o"></i> <a href="#">refimariskap@gmail.com</a></p>
+					<p><i class="fa fa-map-marker"></i> Jl Soekarno Hatta Kota Madiun</p>
+				</div>
+				<div class="col-sm-6 col-md-8">
+					<form action="#" method="post" role="form">
+						<div class="col-md-6">
+							<input type="text" class="form-control" placeholder="Name" name="name"/>
+						</div>
+						<div class="col-md-6">
+							<input type="email" class="form-control" placeholder="Email" name="email" />
+						</div>
+						<div class="col-md-12">
+							<textarea class="form-control" rows="6" placeholder="Message" name="message"></textarea>
+						</div>
+						<div class="col-md-12">
+							<input type="submit" value="SEND MESSAGE" class="form-control">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end templatemo contact -->
+
+	<!-- start templatemo footer -->
+	<footer class="text-center">
+		<div class="container">
+			<div class="row">
+				<div class="social_icon">
+					<a href="#" class="fa fa-facebook"></a>
+					<a href="#" class="fa fa-twitter"></a>
+					<a href="#" class="fa fa-linkedin"></a>
+					<a href="#" class="fa fa-tumblr"></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- end templatemo footer -->
+
+	<!-- start back to top -->
+	<a href="#top" class="go-top"><i class="fa fa-chevron-up fa-1x"></i></a>
+	<!-- end back to top -->
+	
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/jquery.flexslider.js"></script>
+
+	<!-- start templatemo back to top js -->
+	<script>
+		$(document).ready(function() {
+			// FlexSlider 
+		  	$('.flexslider').flexslider({
+		      animation: "fade",
+		      directionNav: false
+		  	});
+
+			// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go-top').fadeIn(200);
+				} else {
+					$('.go-top').fadeOut(200);
+				}
+			});		
+			// Animate the scroll to top
+			$('.go-top').click(function(event) {
+				event.preventDefault();
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+		});
+	</script>
+	<!-- end templatemo back to top js -->
+
+	</body>
+</html>
